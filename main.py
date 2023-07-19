8debc491b2d4ee3ef8a9c9f654c56d088baa5969
nikzdevz <nikzdreamer2001@gmail.com>
2023-07-19 18:59:23 +0530

    initial Website Ui and updated css

--- main.py
+++ main.py
@@ -1,12 +1,44 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        json_file = "sitedata.json"
+        new_data = {
+            "id": request.form['id'],
+            "site": request.form['site'],
+            "other": request.form['other']
+        }
+        existing_data = []
+        try:
+            with open(json_file) as file:
+                existing_data = json.load(file)
+        except FileNotFoundError:
+            pass
+        # Append the new dictionary to the existing data
+        existing_data.append(new_data)
+        # Write the updated data back to the JSON file
+        with open(json_file, "w") as file:
+            json.dump(existing_data, file)
+    return redirect('/')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    existing_data = []
+    json_file = "sitedata.json"
+    try:
+        with open(json_file) as file:
+            existing_data = json.load(file)
+    except FileNotFoundError:
+        pass
+    return render_template('index.html', regData=existing_data)
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     app.run(debug=True)
