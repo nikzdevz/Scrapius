10ed69bcb80d20b2bf203739c7b20d6436a520d1
nikzdevz <nikzdreamer2001@gmail.com>
2023-08-01 15:06:28 +0530

    Check Later

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
+@app.route('/manageSite', methods=['GET', 'POST'])
+def manageSite():
+    json_file = "sitedata.json"
+    existing_data = []
+    try:
+        with open(json_file) as file:
+            existing_data = json.load(file)
+    except FileNotFoundError:
+        pass
+    return render_template('manageSite.html', regData=existing_data)
+
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
     if request.method == 'POST':
         json_file = "sitedata.json"
         new_data = {
-            "id": request.form['id'],
-            "site": request.form['site'],
-            "other": request.form['other']
+            "url": request.form['siteBox'],
+            "schema": request.form['schemaBox']
         }
         existing_data = []
         try:
@@ -25,7 +35,7 @@ def submit():
         # Write the updated data back to the JSON file
         with open(json_file, "w") as file:
             json.dump(existing_data, file)
-    return redirect('/')
+    return redirect('/manageSite')
 
 
 @app.route('/', methods=['GET', 'POST'])
